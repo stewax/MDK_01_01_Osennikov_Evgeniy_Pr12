c4f3e66e2e21bcd5399537b227209bc1fd0988ff
Anna <aneckalarionova@gmail.com>
2025-10-20 17:54:02 +0500

    Создание класса Author. Osennikov

---
+++ Classes/Author.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book_Osennikov.Classes
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string FIO { get; set; }
+        public Author(int Id, string FIO)
+        {
+            this.Id = Id;
+            this.FIO = FIO;
+        }
+
+        public static List<Author> AllAuthors()
+        {
+            List<Author> allAuthors = new List<Author>();
+            allAuthors.Add(new Author(1,"Виктор Плевин"));
+            allAuthors.Add(new Author(1, "Александра Маринина"));
+            allAuthors.Add(new Author(1, "Ольга Герр"));
+            return allAuthors;
+        }
+    }
+}
